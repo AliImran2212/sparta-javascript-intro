2f915fc8756be2522e8f5b479710d4483dd14198
Ali <AImran@spartagloba.com>
2019-01-11 15:08:48 +0000

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-// alert ("Hello world");
-console.log("Hi in console");
-var myNumber = 90;
-console.log(myNumber);
-var mNumber = 45;
-console.log(myNumber+mNumber);
-var Hnhumber = 80;
-console.log(Hnhumber*mNumber);
-var nnumer="Hey";
-var number2="23";
-console.log("Number"+number2+nnumer);
+// // Intiger
+// var int1 = 1;
+// var int2 = 2;
+
+// console.log(int1 +int2)
+
+// // Float
+// var Float = 2.3;
+// var float2 =12.67;
+
+// console.log(Float1 = float2);
+// console.log(Math.PI)
+
+// strings
+// var str1 = "This is a string 1";
+// var str2 = "this is a string2";
+// var str3 - "this is string 3";
+
+// var conCat = str1 + "hello world";
+// var interpolated = "string 3: ${str3}"
+
+// console.log(conCat);
+
+// Boolean
+var bool1 = true;
+var bool2 = false;
+
+console.log(bool1 + bool2);
+
